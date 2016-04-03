a7f3027c65cd3b01ff4ebd4dc4fda1adf087fbab
lalit rajendran <lalit.1308@gmail.com>
2016-04-03 18:20:18 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "utility.h"
+
+int main(int argc, char * argv[])
+{
+	printf("hello C\n");
+	printf("sum(5,3): %d\n",sum(5,3));
+	return 0;
+}
---
+++ utility.h
@@ -0,0 +1,5 @@
+#ifndef UTILITY_H_
+#define UTILITY_H_
+int sum(int a, int b);
+// returns the sum of a and b
+#endif /*UTILITY_H_*/
